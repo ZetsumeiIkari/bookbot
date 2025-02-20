fb945be49210b6371bb695e0fb6c7fba86ab484d
ZetsumeiIkari <matthew@mackarill.co.uk>
2025-02-20 00:36:12 +0000

    impoved code

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def main():
-    path_to_file = "books/frankenstein.txt"
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        #print(file_contents)
-        count(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words (text)
+    print (f"{num_words} words in {book_path}")
 
-def count(file_contents):
-    words = file_contents.split()
-    print (len(words))
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 main()
