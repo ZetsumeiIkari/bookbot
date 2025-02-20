15c185b75b1d4639a565abd9ceb3b60cb22ad178
ZetsumeiIkari <matthew@mackarill.co.uk>
2025-02-20 01:30:28 +0000

    letter count

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words (text)
     print (f"{num_words} words in {book_path}")
-
+    print (char_count(text))
+     
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -11,5 +12,13 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    char_list = list(text.lower())
+    char_set = set(char_list)
+    output = {}
+    for c in char_set:
+        output.update({c : (char_list.count(c))})
+    return output
 
 main()
