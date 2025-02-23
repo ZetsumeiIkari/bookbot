596b22d540ada920fbb13166a8cb9a970036f8d6
ZetsumeiIkari <matthew@mackarill.co.uk>
2025-02-23 23:16:25 +0000

    simplified gitignore

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words (text)
-    print (f"{num_words} words in {book_path}")
-    print (char_count(text))
+    num_chars = char_count(text)
+    
      
 def get_book_text(path):
     with open(path) as f:
