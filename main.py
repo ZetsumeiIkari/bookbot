82a0917e5051f656cb3da11586b224356a431e9d
ZetsumeiIkari <matthew@mackarill.co.uk>
2025-02-24 01:18:21 +0000

    final finished project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars_dict, sort
 
 def get_book_text(path):
@@ -5,11 +6,16 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     sorted = sort(chars_dict)
+    
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
